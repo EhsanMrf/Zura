2679f52ac809ac0a8dc312e320b90b51231548b2
EhsanMrf <developermrf@gmail.com>
2025-04-21 22:20:53 +0330

    inf config and install packages

---
+++ Zura/Zura.Infrastructure.Persistence/Configuration/TodoConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Zura.Domain.Model.Todos.Entity;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Zura.Infrastructure.Persistence.Configuration;
+
+internal class TodoConfiguration : IEntityTypeConfiguration<Todo>
+{
+    public void Configure(EntityTypeBuilder<Todo> builder)
+    {
+        builder.ToTable("Todos");
+
+        builder.Property(t => t.Title)
+            .HasMaxLength(150)
+            .IsRequired();
+
+        builder.Property(t => t.Description)
+            .HasMaxLength(300);
+
+        builder.OwnsOne(t => t.Status, status =>
+        {
+            status.Property(x=>x.Status)
+                .HasColumnName("Status")
+                .IsRequired();
+        });
+    }
+}
+
---
+++ Zura/Zura.Infrastructure.Persistence/UnitOfWork/ApplicationUnitOfWork.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zura.Application.Common.UnitOfWork;
+using Zura.Application.Common;
+using Zura.Domain.Model.Todos.Entity;
+
+namespace Zura.Infrastructure.Persistence.UnitOfWork
+{
+    public  class ApplicationUnitOfWork(ApplicationDbContext applicationDbContext) : IApplicationUnitOfWork
+    {
+        public async Task<Result> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            try
+            {
+                await applicationDbContext.SaveChangesAsync(cancellationToken);
+                return Result.Success();
+            }
+            catch (DbUpdateConcurrencyException e)
+            {
+                //If you want to do something
+                return Result.Failure(e.Message);
+            }
+            catch (DbUpdateException e)
+            {
+                return Result.Failure(e.Message);
+            }
+        }
+
+        public void Dispose()
+        {
+            applicationDbContext.Dispose();
+            GC.SuppressFinalize(this);
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            await applicationDbContext.DisposeAsync();
+            GC.SuppressFinalize(this);
+        }
+
+        public DbSet<Todo> Todos => applicationDbContext.Set<Todo>();
+    }
+}
