b64927c27f92d2e55381a8cae0d1a205b9056c87
EhsanMrf <developermrf@gmail.com>
2025-04-21 21:12:32 +0330

    configuration for application

---
+++ Zura/Zura.Application/Common/UnitOfWork/IApplicationUnitOfWork.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Zura.Domain.Model.Todo.Entity;
+
+namespace Zura.Application.Common.UnitOfWork;
+
+public interface IApplicationUnitOfWork : IUnitOfWork
+{
+    public DbSet<Todo> Todos { get; }
+}
---
+++ Zura/Zura.Application/Common/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,6 @@
+﻿namespace Zura.Application.Common.UnitOfWork;
+
+public interface IUnitOfWork : IDisposable, IAsyncDisposable
+{
+    public Task<Result> SaveChangesAsync(CancellationToken cancellationToken = default);
+}
