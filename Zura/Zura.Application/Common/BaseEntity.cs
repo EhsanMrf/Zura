7e05432223d2dc03dd20eb397eca182eb7bd42ab
EhsanMrf <developermrf@gmail.com>
2025-04-21 18:57:39 +0330

    base Entity with Id struct

---
+++ Zura/Zura.Application/Common/BaseEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace Zura.Application.Common;
+
+internal abstract class BaseEntity<T> where T : struct
+{
+
+    public T Id { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+    public bool IsDeleted { get; set; }
+}
